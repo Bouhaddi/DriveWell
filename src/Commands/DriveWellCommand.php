de733ebfef97d265db9e70a96e679f7ef66de093
bouhaddi.career@gmail.com <bouhaddi.career@gmail.com>
2023-09-01 11:00:40 -0700

    Create the domain architecture and Files Architecture

--- src/Commands/DriveWellCommand.php
+++ src/Commands/DriveWellCommand.php
@@ -3,17 +3,20 @@
 namespace Bouhaddi\DriveWell\Commands;
 
 use Illuminate\Console\Command;
+use Bouhaddi\DriveWell\DriveWell;
 
 class DriveWellCommand extends Command
 {
-    public $signature = 'drivewell';
+    public $signature = 'make:domain {cmd}';
 
-    public $description = 'My command';
+    public $description = 'Generate a Domain Driven Design Skeleton';
 
-    public function handle(): int
+    public function handle()
     {
-        $this->comment('All done');
+        $bootstrap = new DriveWell();
+        $domainName = $this->argument('cmd');
+        $bootstrap->generateDomainSkeleton($domainName);
 
-        return self::SUCCESS;
+        $this->info("DDD Skeleton for $domainName generated successfully");
     }
 }
--- src/DriveWell.php
+++ src/DriveWell.php
@@ -4,4 +4,39 @@ namespace Bouhaddi\DriveWell;
 
 class DriveWell
 {
+    protected $folders = ["Models", "Controllers", "Services", "Repositories", "Routes"];
+    public function generateDomainSkeleton(string $domain)
+    {
+        $this->createDomainDirectory($domain);
+
+        $this->createDomainArchitecture($domain);
+    }
+
+    protected function createDomainDirectory(string $domain)
+    {
+        $domain = ucfirst($domain);
+        $domainDirectory = base_path("app/Domain/$domain");
+        if(!file_exists($domainDirectory)){
+            mkdir($domainDirectory, 0755, true);
+        }
+    }
+
+    private function createDomainArchitecture(string $domain)
+    {
+        foreach($this->folders as $folder){
+            $folderPath = base_path("app/Domain/$domain/$folder");
+            if(!file_exists($folderPath)){
+                mkdir($folderPath, 0755, true);
+            }
+        }
+
+        $this->createDomainFiles($domain);
+    }
+
+    private function createDomainFiles(string $domain)
+    {
+
+    }
+
+
 }
