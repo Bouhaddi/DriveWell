1e57b8616bbf2c00f2264d4fd983e33040402c67
bouhaddi.career@gmail.com <bouhaddi.career@gmail.com>
2023-09-02 10:13:42 -0700

    remove comments

--- src/DriveWell.php
+++ src/DriveWell.php
@@ -19,13 +19,13 @@ class DriveWell
      */
     public function generateDomainSkeleton(string $domain, array $options): void
     {
-//        $this->createDomain($domain);
-//
-//        foreach ($options as $folder => $className) {
-//            if ($className) {
-//                $this->createFiles($domain, $folder, $className);
-//            }
-//        }
+        $this->createDomain($domain);
+
+        foreach ($options as $folder => $className) {
+            if ($className) {
+                $this->createFiles($domain, $folder, $className);
+            }
+        }
     }
 
     /**
@@ -36,16 +36,16 @@ class DriveWell
      */
     protected function createDomain(string $domain): void
     {
-//        $domain = ucfirst($domain);
-//        $domainDirectory = base_path("app/Domain/$domain");
-//
-//        if (!file_exists($domainDirectory)) {
-//            mkdir($domainDirectory, 0755, true);
-//
-//            foreach ($this->folders as $folder) {
-//                mkdir("$domainDirectory/$folder", 0755, true);
-//            }
-//        }
+        $domain = ucfirst($domain);
+        $domainDirectory = base_path("app/Domain/$domain");
+
+        if (!file_exists($domainDirectory)) {
+            mkdir($domainDirectory, 0755, true);
+
+            foreach ($this->folders as $folder) {
+                mkdir("$domainDirectory/$folder", 0755, true);
+            }
+        }
     }
 
     /**
@@ -57,27 +57,27 @@ class DriveWell
      */
     protected function createFiles(string $domain, string $folder, string $className): void
     {
-//        $domain = Str::ucfirst($domain);
-//        $folder = Str::ucfirst($folder);
-//        $className = Str::ucfirst($className);
-//        $namespace = "App\Domain\\$domain\\" . Str::plural($folder);
-//        $filePath = "$namespace/$className.php";
-//
-//        if (File::exists($filePath)) {
-//            return; // File already exists, no need to create it again
-//        }
-//
-//        $fileSkeleton = file_get_contents(__DIR__ . "/Stubs/$folder.stub"); // Replace with the correct path to your stubs directory
-//
-//        if ($fileSkeleton !== false) {
-//            $fileContent = str_replace(
-//                ["{{ Namespace }}", "{{ ClassName }}"],
-//                [$namespace, $className],
-//                $fileSkeleton
-//            );
-//
-//            File::put($filePath, $fileContent);
-//        }
+        $domain = Str::ucfirst($domain);
+        $folder = Str::ucfirst($folder);
+        $className = Str::ucfirst($className);
+        $namespace = "App\Domain\\$domain\\" . Str::plural($folder);
+        $filePath = "$namespace/$className.php";
+
+        if (File::exists($filePath)) {
+            return; // File already exists, no need to create it again
+        }
+
+        $fileSkeleton = file_get_contents(__DIR__ . "/Stubs/$folder.stub"); // Replace with the correct path to your stubs directory
+
+        if ($fileSkeleton !== false) {
+            $fileContent = str_replace(
+                ["{{ Namespace }}", "{{ ClassName }}"],
+                [$namespace, $className],
+                $fileSkeleton
+            );
+
+            File::put($filePath, $fileContent);
+        }
     }
 
     // remove domain
